40e524681ff4c7904abd91c60b6f8441f6963963
Haowei <z5258575@ad.unsw.edu.au>
2020-02-21 15:16:43 +1100

    some prolog codes

--- week1/lec2.pl
+++ week1/lec2.pl
@@ -1,3 +1,23 @@
 later(date(Y,M,D1),date(Y,M,D2)) :- D1>D2.
 later(date(Y,M1,_),date(Y,M2,_)) :- M1>M2.
 later(date(Y1,_,_),date(Y2,_,_)) :- Y1>Y2.
+
+on(a,b).
+on(b,c).
+on(c,d).
+above(B1,B2) :-
+    on(B1,B2).
+above(B1,B2) :-
+    on(B1,B),
+    above(B,B2).
+
+conc([],X,X).
+conc([A|B],C,[A|D]):-conc(B,C,D).
+
+
+
+
+
+
+
+
