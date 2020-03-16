2798edac52867d0edcd7f0c247f5406aae50fe05
Haowei <z5258575@ad.unsw.edu.au>
2020-03-16 13:25:39 +1100

    finish nearly

--- week2/tut.pl
+++ week2/tut.pl
@@ -1,3 +1,5 @@
 insert(Num,[],[Num]).
 insert(NUM,[Head|REST1],[Head|REST2]):-NUM>Head,insert(NUM,REST1,REST2).
 insert(Num,[Head|REST],[NUM,Head|REST]):-insert(NUM,[],NUM).
+
+predicat_split(BigList,List1,List2):
